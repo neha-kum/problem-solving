328a294f19e47a001dec8118f7103e65958fe652
Neha Kumari <96180294+neha-kum@users.noreply.github.com>
2022-10-09 11:39:09 +0530

    Update Matrix.java

--- Matrix Operation/Matrix.java
+++ Matrix Operation/Matrix.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Matrix
 {
 	public static void main(String[] args) {
-		int a[][]= new int[100][100];
 		Scanner sc= new Scanner(System.in);
 		System.out.print("Enter the no. of rows : " );
 		int row = sc.nextInt();
 		System.out.print("Enter the no. of columns : ");
 		int column= sc.nextInt();
+		int a[][]= new int[row+1][column+1];
+	
 		
 		for(int i=1;i<=row;i++){
 		    for(int j=1;j<=column;j++){
@@ -17,6 +18,7 @@ public class Matrix
 		        
 		    }
 		}
+		System.out.println("Matrix A : ");
 		for(int i=1;i<=row;i++){
 		    for(int j=1;j<=column;j++){
 		        System.out.print(a[i][j]+ "\t");
@@ -25,5 +27,6 @@ public class Matrix
 		}
 	}
 }
+
 
 
