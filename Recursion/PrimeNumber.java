ae6ce2c722cc19d881b63d06ec0a0f7c5dff68b6
Neha Kumari <96180294+neha-kum@users.noreply.github.com>
2022-11-01 14:00:04 +0530

    Add files via upload

---
+++ Recursion/PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber
+{
+    public static boolean isPrime(int n, int i){
+        if(n<=2)
+        return (n==2)? true : false;
+        
+        if(n%i==0)
+        return false;
+        
+        if(i*i>n)
+        return true;
+        
+        return isPrime(n,i+1);
+    }
+    
+	public static void main(String[] args) {
+	    int n = 7;
+	    if(isPrime(n,2))
+	    System.out.println("It is a prime no.");
+	    else 
+	    System.out.println("It is not a prime no.");
+	}
+}
+
