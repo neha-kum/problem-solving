8803df9fb5a06cfd108caf088454a6a73122f69b
Neha Kumari <96180294+neha-kum@users.noreply.github.com>
2022-11-05 01:17:27 +0530

    Add files via upload

---
+++ Array/FindLargestElement.java
@@ -0,0 +1,21 @@
+public class FindLargestElement {
+
+    public static int findMax(int A[], int n)
+    {
+  
+      if(n == 1)
+        return A[0];
+         
+        return Math.max(A[n-1], findMax(A, n-1));
+    }
+ 
+    // Driver code
+    public static void main(String args[])
+    {
+        int A[] = {1, 4, 45, 6, -50, 10, 2};
+
+
+        System.out.println(findMax(A, A.length));
+    }
+}
+
